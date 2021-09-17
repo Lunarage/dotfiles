4a49177be4aba2bd32d860e3538247a00fec032e
Magne Halvorsen <mh95244719@gmail.com>
2021-09-17 18:54:30 +0200

    Dynamic ethernet in polybar

--- polybar-launch.sh
+++ polybar-launch.sh
@@ -9,10 +9,13 @@ killall -q polybar
 # Launch Polybar, using default config location ~/.config/polybar/config
 # polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
 
-# Find all moitors
+# Find Ethernet
+ETH=$(ip link | grep -o -e "enp[0-9a-z]*" -e "state UP" | head -n 1)
+
+# Find all monitors
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload mybar 2>&1 | tee -a /tmp/polybar.log & disown &
+        ETH=$ETH MONITOR=$m polybar --reload mybar 2>&1 | tee -a /tmp/polybar.log & disown &
     done
 else
     polybar --reload mybar 2>&1 | tee -a /tmp/polybar.log & disown &
